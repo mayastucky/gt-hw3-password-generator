5afd518b3f0211a062d29bcdf3b4ddfcd9d9cadb
Maya Stucky <mayaatstucky@gmail.com>
2020-06-07 16:17:13 -0400

    added readme, more comments, and uploaded screenshot of app

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # gt-hw3-password-generator
+
+## Description
+
+This program takes a user's desired password length and criteria and outputs a randomized and secure password for protecting themselves and their information.
+
+## Installation
+
+Clone the repo and run the index.html in your browser.
+
+## Usage
+
+This application is perfect for those who need to secure their information with an array (haha!) of different and secure passwords. Once you open the app, click on the button to have the browser prompt you to select your desired length (8-128 characters) and select at least one character type. You will then be alerted with your secure password.
+
+<img src = "app.png" alt = "image of application" >
+
+## Credits
+
+This was a solo project completed as homework for the Georgia Tech Full-Stack Flex Program. As always, many thanks to my peers who are always willing to work out the kinks together.
+
+## License
+
+MIT License
+
+Copyright (c) [2020][maya stucky]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## URL
+
+<a href = "https://mayastucky.github.io/gt-hw3-password-generator/">Link to Deployed Site</a>
+
+<a href = "https://github.com/mayastucky/gt-hw3-password-generator">Link to GitHub Repo</a>
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Assignment Code
 //generateBtn is assigned to the button from HTML
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
+  // prompt user to enter their desired password length
   var passwordLength = prompt(
     "Please enter desired password length between 8 and 128 characters"
   );
-
+  //have function run again if it does not meet requirements
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a password between 8 and 128 characters");
     return writePassword();
   }
-  //function writePassword() {
+  //asks user to select "ok" or "cancel" for each type of character
   var uppers = confirm("Would you like uppercase letters in your password?");
   var lowers = confirm("Would you like lower case numbers in your password?");
   var numbers = confirm("Would you like numbers in your password?");
   var special = confirm("Would you like special characters in your password?");
-
+  //array of upper case letters
   var upperArray = [
     "A",
     "B",
@@ -46,6 +46,7 @@ function writePassword() {
     "Y",
     "Z",
   ];
+  //array of all lower case letters
   var lowerArray = [
     "a",
     "b",
@@ -74,34 +75,39 @@ function writePassword() {
     "y",
     "z",
   ];
-
+  //array of all numbers
   var intArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
+  //array of special characters
   var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-
+  //empty array
   var userChoices = [];
-
+  //if the user selects upper case, this if will push upper case letter array into the empty array
   if (uppers) {
     userChoices = userChoices.concat(upperArray);
   }
+  //if the user selects lower case letters, this will push lower case letter array into the array
   if (lowers) {
     userChoices = userChoices.concat(lowerArray);
   }
+  //if the user selects numbers, this will push numbers into the password array
   if (numbers) {
     userChoices = userChoices.concat(intArray);
   }
+  //if the user selects special characters, this will push special characters into the password array
   if (special) {
     userChoices = userChoices.concat(specialArray);
   }
-
+  //if the password array ends up being empty, the user will be asked to select at least one type and the function will run again
   if (userChoices.length === 0) {
     alert("Please select at least one character type.");
     writePassword();
   }
-
   console.log(userChoices);
+  //this is the empty password variable
   var password = "";
-
+  //for loop that will loop as many times as the user has indicated
+  //this for loop selects a random index from inside the userChoices array and concatenates it
+  //after the loop is run it will return the password to the user in an alert
   for (var i = 0; i < passwordLength; i++) {
     var randomPasswordNum = Math.floor(Math.random() * userChoices.length);
     console.log(randomPasswordNum);
@@ -109,6 +115,7 @@ function writePassword() {
     console.log(password);
   }
   alert("Your secure password is: " + password);
+
   //}
 
   // function generatePassword() stored as var password within function writePassword
